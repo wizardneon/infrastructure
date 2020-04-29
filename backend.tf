b14f207fb79253e6f44faf904263408c8dd550de
itsmeoz <onur.ozkan83@gmail.com>
2020-04-29 22:22:58 +0300

    terraform state file will be store in S3 bucket

---
+++ backend.tf
@@ -0,0 +1,18 @@
+# S3 bucket for terroform states
+terraform {
+  backend "s3" {
+    bucket = "tf-states-for-eks"
+    key    = "states/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+# Retrieve state data from S3
+data "terraform_remote_state" "state" {
+  backend = "s3"
+  config = {
+    bucket               = "tf-states-for-eks"
+    key                  = "states/terraform.tfstate"
+    region               = "eu-west-1"
+  }
+}
