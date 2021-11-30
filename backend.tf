d7bc2bf3c4fca614acdf2ad2599da383b1629cb0
wizardneon <wizardneon@gmail.com>
2021-11-30 11:58:16 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -12,7 +12,7 @@ terraform {
 data "terraform_remote_state" "state" {
   backend = "s3"
   config = {
-    bucket               = "tf-states-for-eks"
+    bucket               = "state-teraform"
     key                  = "states/terraform.tfstate"
     region               = "eu-west-1"
     profile = "wizardneon"
