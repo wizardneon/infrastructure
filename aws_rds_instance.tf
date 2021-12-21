d1b070d746e76ec43c49811bd7de016994fecc8e
wizardneon <wizardneon@gmail.com>
2021-12-21 09:15:26 +0000

    Update aws_rds_instance.tf

--- aws_rds_instance.tf
+++ aws_rds_instance.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "diplomrds" {
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   vpc_security_group_ids      =["${aws_security_group.rds_sg.id}"]
-  name                 = ${database_name}
-  username             = ${database_user}
-  password             = $[database_password}
+  name                 = "$database_name"
+  username             = "$database_user"
+  password             = "$database_password"
 }
