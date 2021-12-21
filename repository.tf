95a613a076dbc55f4cbf46a067d15b3405cbe321
wizardneon <wizardneon@gmail.com>
2021-12-21 09:05:41 +0000

    Update repository.tf

--- repository.tf
+++ repository.tf
@@ -1,20 +1,5 @@
 # ECR repo
-resource "aws_ecr_repository" "tf-eks-ecr" {
+resource "aws_ecr_repository" "ecr_for_images" {
   name = "${var.repo_name}"
 }
 
-# random string for bucket name
-resource "random_string" "suffix" {
-  length  = 5
-  upper   = false
-  lower   = true
-  number  = false
-  special = false
-}
-
-# pipeline artifact bucket
-resource "aws_s3_bucket" "build_artifact_bucket" {
-  bucket        = "${var.repo_name}-${random_string.suffix.result}"
-  acl           = "private"
-  force_destroy = "true"
-}
