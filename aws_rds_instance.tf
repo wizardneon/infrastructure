b842f44a1986b705534cbee5f9b7be4f0b674553
wizardneon <wizardneon@gmail.com>
2021-12-22 11:19:38 +0000

    Update aws_rds_instance.tf

--- aws_rds_instance.tf
+++ aws_rds_instance.tf
@@ -4,8 +4,8 @@ resource "aws_db_instance" "postgres" {
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   vpc_security_group_ids  = ["${aws_security_group.rds_sg.id}"]
-  db_subnet_group_name = ["${aws_db_subnet_group.db_subnet[1].name}", "${aws_db_subnet_group.db_subnet[0].name}"]
-   name                 = var.DATABASE_NAME
+  db_subnet_group_name = ["${aws_db_subnet_group.db_subnet.name}"]
+   name                = var.DATABASE_NAME
   username             = var.DATABASE_USER
   password             = var.DATABASE_PASSWORD
   skip_final_snapshot  = true
