88925b2e589104c0a26e08fc157c22ec9b307165
wizardneon <wizardneon@gmail.com>
2021-12-25 06:57:19 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,15 +5,5 @@ terraform {
     key    = "states/terraform.tfstate"
     region = var.aws_region
     profile = var.profile
+ }
 }
-
-# Retrieve state data from S3
-#data "terraform_remote_state" "state" {
-#  backend = "s3"
-#  config = {
-#    bucket               = "state-teraform"
-#    key                  = "states/terraform.tfstate"
-#    region               = "eu-west-1"
-#    profile = "wizardneon"
-# }
-#}
