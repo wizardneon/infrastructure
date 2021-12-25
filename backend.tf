331886c3813a3c0573252e22a2c1ca4c9dcc344f
wizardneon <wizardneon@gmail.com>
2021-12-25 07:04:26 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket = "state-teraform"
     key    = "states/terraform.tfstate"
-    region = var.aws_region
-    profile = var.profile
+    region = "var.aws_region"
+    profile = "var.profile"
  }
 }
