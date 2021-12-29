ac5f37a8ce7fd6b622235a202ac32ebfbb65f119
wizardneon <wizardneon@gmail.com>
2021-12-29 02:15:39 +0000

    Update sec_groups.tf

--- sec_groups.tf
+++ sec_groups.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "rds_sg" {
     from_port = 5432
     to_port   = 5432
     protocol  = "tcp"
-    security_groups = aws_security_group.k8s-worker-node
+    security_groups = ["${aws_security_group.k8s-worker-node.id}"]
   }
   
   egress {
