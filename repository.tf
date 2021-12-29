d101b3945e4ee3d3bbb8361fd9941b35c411ee39
wizardneon <wizardneon@gmail.com>
2021-12-29 02:40:14 +0000

    Update repository.tf

--- repository.tf
+++ repository.tf
@@ -2,5 +2,9 @@
 resource "aws_ecr_repository" "ecr_for_images" {
   name = "${var.repo_name}"
   image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+  scan_on_push = true
+  }
 }
 
