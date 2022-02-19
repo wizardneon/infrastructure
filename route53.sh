06d2cae4650333fec0a37a30bb55d25d07be3806
wizardneon <wizardneon@gmail.com>
2022-02-19 16:40:41 +0000

    Update route53.sh

--- route53.sh
+++ route53.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-
-hostedzoneroute=$(aws route53 list-hosted-zones | grep hostedzone | cut -c32-52)
-
-hostedzonenlb=\"$(aws route53 list-resource-record-sets --hosted-zone-id $hostedzoneroute | grep HostedZoneId | cut -c34-47)\"
-
-nlb=\"$(kubectl get svc -n ingress-nginx | grep LoadBalancer | cut -c70-146)\"
-
+aws route53 list-hosted-zones | grep hostedzone > 1.txt && cat 1.txt
+hostedzoneroute=$(sed -e "s/^.\{,31\}//;s/.\{,2\}$//" 1.txt)
+echo $ hostedzoneroute
+aws route53 list-resource-record-sets --hosted-zone-id $hostedzoneroute
+aws route53 list-resource-record-sets --hosted-zone-id $hostedzoneroute | grep HostedZoneId > 2.txt
+cat 2.txt
+hostedzonenlb=\"$(sed -e "s/^.\{,33\}//;s/.\{,2\}$//" 2.txt)\"
+echo $hostedzonenlb
+kubectl get svc -n ingress-nginx | grep amazonaws | awk '{ print $4 }'
+nlb=\"$(kubectl get svc -n ingress-nginx | grep amazonaws | awk '{ print $4 }')\"
+echo $nlb
 aws route53 change-resource-record-sets \
     --hosted-zone-id /hostedzone/$hostedzoneroute\
     --change-batch \
-     '{"Changes": [ { "Action": "UPSERT", "ResourceRecordSet": { "Name": "wizardneon.link", "Type": "A", "AliasTarget":{ "HostedZoneId": '$hostedzonenlb',"DNSName": '$nlb',"EvaluateTargetHealth": false} } } ]}'
+     '{"Changes": [ { "Action": "UPSERT", "ResourceRecordSet": { "Name": "wizardneon.link", "Type": "A", "AliasTarget":{ "HostedZoneId": '$hostedzonenlb',"DNSName": '$nlb',"EvaluateTargetHealth":
+false} } } ]}'
