a27ad8626acc4c254da63ee8282bdd8e836a63b3
wizardneon <wizardneon@gmail.com>
2022-02-21 01:39:25 +0000

    Update get_thumbprint.sh

--- get_thumbprint.sh
+++ get_thumbprint.sh
@@ -3,4 +3,4 @@
 
 THUMBPRINT=$(echo | openssl s_client -servername oidc.eks.${1}.amazonaws.com -showcerts -connect oidc.eks.${1}.amazonaws.com:443 2>&- | tac | sed -n '/-----END CERTIFICATE-----/,/-----BEGIN CERTIFICATE-----/p; /-----BEGIN CERTIFICATE-----/q' | tac | openssl x509 -fingerprint -sha1 -noout | sed 's/://g' | awk -F= '{print tolower($2)}')
 THUMBPRINT_JSON="{\"thumbprint\": \"${THUMBPRINT}\"}"
-echo $THUMBPRINT_JSON
+echo $THUMBPRINT_JSON1
